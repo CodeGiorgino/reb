f28e754438d99c79cba55bc0c95a11a2d049bb88
CodeGiorgino <andreagiorgino1@gmail.com>
2024-06-30 20:48:42 +0200

    Build fixed

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ mkdir -p obj
 mkdir -p build
 
 GXX='g++'
-CXX='-Wall -Wextra --std=c++23 -c'
+CXX='-Wall -Wextra --std=c++23'
 CC=''
 
 for f in "$(find './src' -type f -path '*.cpp')"; do
     base="$(basename "${f}")"
     dest="./obj/${base%.*}.o"
-    $GXX $CXX $CC -o "${dest}" "${f}"
+    $GXX $CXX $CC -c -o "${dest}" "${f}"
 done
 
-$GXX $CXX $CC './build/reb' "$(find './obj' -type f -path '*.o')"
+$GXX $CXX $CC -o './build/reb' "$(find './obj' -type f -path '*.o')"
 cp -r './assets' './build/'
