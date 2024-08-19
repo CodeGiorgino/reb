0d6210502252c0790662c18972ccb37651b97300
CodeGiorgino <andreagiorgino1@gmail.com>
2024-08-19 17:08:04 +0200

    release installation script

--- install.sh
+++ install.sh
@@ -16,25 +16,26 @@ mkdir -p ~/.config/reb/models
 
 # curl -o ~/.config/reb/models/models.tar.gz https://raw.githubusercontent.com/CodeGiorgino/reb/master/assets/models.tar.gz
 # tar -xvzf ~/.config/reb/models/models.tar.gz
-cp assets/cpp.config ~/.config/reb/models/
+cp assets/cpp.json ~/.config/reb/models/
 
 GXX='g++'
-CXX='-Wall -Wextra -std=c++23'
+CXX='-Wall -Wextra -std=c++23 -g'
 
 for f in $(find 'src' -type f -path '*.cpp'); do
     base="$(basename "${f}")"
     dest="obj/${base%.*}.o"
     echo -e "\033[33;1mINFO:\033[0m building ${f}"
-    $GXX $CXX -c -o "${dest}" "${f}"
+    $GXX $CXX -c -O2 -o "${dest}" "${f}"
 done
 
-$GXX $CXX \
+$GXX \
     -o 'build/reb' \
-    $(find 'obj' -type f -path '*.o')
+    $(find 'obj' -type f -path '*.o') \
+    'deps/cppjson.a'
 
 if [ -L '/usr/local/bin/reb' ]
 then
-    echo -e '\033[31;1mERROR:\033[0m cannot create symlink /usr/local/bin/reb: symlink exists'
+    echo -e '\033[31;1mERROR:\033[0m cannot create symlink /usr/local/bin/reb: a symlink with the same name already exists'
     exit 1
 fi
 
